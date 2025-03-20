8ef9def80971f98c3daf7c9aa14abb1e6d01eaaf
Alexander <SaSkrkr123.com@mail.ru>
2025-03-20 17:03:47 +0300

    window

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import Home from './Home'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/Home.tsx
@@ -0,0 +1,18 @@
+import './Home.css'
+const Home = () => {
+    return (
+        <div>
+            <header>
+        <nav className='window'>
+          <div>
+              <a href='/'>CatHotel.com</a>
+          </div>
+        </nav>
+      </header>
+      <div className='img_div'>
+        
+      </div>
+        </div>
+    )
+}
+export default Home
