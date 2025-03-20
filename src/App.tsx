3cb66a2374d0ad36782eb7202879197d3b9ffe63
Alexander <SaSkrkr123.com@mail.ru>
2025-03-20 17:59:54 +0300

    new window

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,17 @@ import './App.css'
 function App() {
   return (
     <div>
+       <header>
+        <nav className='window'>
+          <div>
+              <a href='/'>CatHotel.com</a>
+          </div>
+        </nav>
+      </header>
       <BrowserRouter>
         <Routes>
           <Route index path='/' element={<Home />} />
+          <Route path='/bay' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Home.tsx
+++ src/Home.tsx
@@ -2,17 +2,11 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
-            <header>
-        <nav className='window'>
-          <div>
-              <a href='/'>CatHotel.com</a>
-          </div>
-        </nav>
-      </header>
+           
       <div className='img_div'>
         <img src='./public/img.jpg' />
           <h1 className='text_img'>Отпуск в доме вашей мечты</h1>
-        
+        <a href=''></a>
       </div>
         </div>
     )
